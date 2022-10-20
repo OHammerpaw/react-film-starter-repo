cbbac1758d0130e706f5cefdb6bee4ed2f956fa9
lozier <latayroutier@gmail.com>
2022-10-20 16:48:00 -0400

    added better alt text

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -4,7 +4,7 @@ class Poster extends Component {
     render() {
         return (
             <img src={`https://image.tmdb.org/t/p/w780${this.props.film.poster_path}`} 
-            alt={`${this.props.title}'s movie poster`}/>
+            alt={`${this.props.film.title}'s movie poster`}/>
         )
     }
 }
