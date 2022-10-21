e798471351b17da5a1889e07c3c13fd432aa88f5
lozier <latayroutier@gmail.com>
2022-10-21 16:59:11 -0400

    add 'All' and 'Faves' sub-headings to the film list, made the subheadings clickable, track the filter in state, color the subheading when selected

--- src/Components/FilmList.js
+++ src/Components/FilmList.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow';
 
 class FilmList extends Component {
-  render() {
+
+    state = {
+        filter: 'all'
+    }
+
+    handleFilterClick = (filter) => {
+
+        console.log('handling filter click', filter)
+        this.setState({filter})
+    }
+
+    render() {
         const allFilms = this.props.films.map((film) => {
             return (
                 <FilmRow film={film}
@@ -13,6 +24,17 @@ class FilmList extends Component {
         <div className="film-library">
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
+                <div className="film-list-filters">
+                    <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`} onClick={() => {this.handleFilterClick("all")}}>
+                            ALL
+                        <span className="section-count">{this.props.films.length}</span>
+                    </div>
+                    <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`} onClick={() => {this.handleFilterClick("faves")}}>
+                            FAVES
+                        <span className="section-count">0</span>
+                    </div>
+                 </div>
+
                 {allFilms}
             </div>
         </div>
